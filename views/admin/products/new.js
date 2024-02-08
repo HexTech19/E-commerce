a26cd023676b5f76aef5048a0659f27d756853fc
Onyedikachi Nwanguma <nwangumabimma@gmail.com>
2024-02-08 17:05:36 +0100

    another update

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,10 +4,10 @@ const { getError } = require('../../helpers');
 module.exports = ({ errors }) => {
   return layout({
     content: `
-    <form method="POST">
+    <form method="POST" enctype="multipart/form-data">
     <input placeholder="Title" name="title"/>
     <input placeholder="Price" name="price"/>
-    <input placeholder="file" name="image"/>
+    <input type="file" name="image"/>
     <button>Submit</button>
     </form> 
     `
